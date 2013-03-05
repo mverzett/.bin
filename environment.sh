27c185a41c7ed14fa73fcb79be554b5144fa9aa5
Mauro Verzetti <mverzett@cern.ch>
2013-03-05 10:37:08 -0600

    updates and pruning

--- environment.sh
+++ environment.sh
@@ -1,3 +1,5 @@
+export DOTBIN=$PWD
+
 #general purpose aliases
 alias la='ls -lah --color'
 alias lt='ls -lrth --color'
@@ -5,14 +7,12 @@ alias lc='ls -h --color'
 alias ll='ls -lh --color'
 
 #.bin specific aliases
-alias web='~/.bin/web.py'
-alias hide='~/.bin/hide.sh'
-alias show='~/.bin/reveal.sh'
+alias web='$DOTBIN/web.py'
+alias hide='$DOTBIN/hide.sh'
+alias show='$DOTBIN/reveal.sh'
 alias term='kill -s SIGINT'
-alias pyRoot="python -i -c 'execfile(\"/afs/hep.wisc.edu/home/mverzett/.bin/pyroot.py\")'"
+alias pyRoot="python -i -c 'execfile(\"$DOTBIN/pyroot.py\")'"
 alias pydbg="python -i -c 'execfile(\"/afs/cern.ch/user/m/mverzett/.bin/pyDebug.py\")'"
 alias sc='scram b -j 4' 
-alias startCrab='source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh'
-alias serialHadd='~/.bin/serialHadd.sh'
-alias extDiff='~/.bin/extended_diff.sh'
-alias rightLumi='lumiCalc2.py -norm pp7TeV overview -i'
+alias serialHadd='$DOTBIN/serialHadd.sh'
+alias rootfind='$DOTBIN/rootfind.py'
