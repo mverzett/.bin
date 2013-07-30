31bfdbd700d561cab3d6619dbecdea4e9401211e
Mauro Verzetti <mverzett@cern.ch>
2013-07-30 06:56:02 -0500

    takes a hisograms and prints the content in standard output

---
+++ hist2txt.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from optparse import OptionParser
+
+def odg(num):
+    num = abs(num)
+    #print num
+    if num == 0.:
+        return 0
+    if num < 10 and num >=1:
+        return 0
+    else:
+        if num > 1:
+            return 1+odg(num/10.)
+        else:
+            return -1+odg(num*10.)
+
+def prettyfloat(value, lenght):
+    odm = odg(value)
+    accuracy = '0' if odm > 1 else \
+               '1' if odm > -1 else \
+               str(1-odm)
+    ret = (('%.'+accuracy+'f') % value ).center(lenght)
+    if len(ret) > lenght:
+        ret = ('%.1e' % value ).center(lenght)
+    return ret
+
+def h1d_to_txt(histo, options):
+    xbins = histo.GetNbinsX()
+    size  = 9
+    centers = ['uflow'.center(size)]
+    vals    = [prettyfloat(histo.GetBinContent(0), size)]
+    errs    = [prettyfloat(histo.GetBinError(0), size)]
+
+    for i in xrange( 1, xbins+1 ):
+        centers.append( prettyfloat( histo.GetXaxis().GetBinCenter(i), size) )
+        vals.append( prettyfloat( histo.GetBinContent(i), size) )
+        errs.append( prettyfloat( histo.GetBinError(i), size) )
+
+    centers.append( 'oflow'.center(size) )
+    vals.append( prettyfloat( histo.GetBinContent(xbins+1), size) )
+    errs.append( prettyfloat( histo.GetBinError(xbins+1), size) )
+
+    firstline = '|'.join(vals)
+    print firstline
+    if options.stat:
+        print '|'.join( '+/-'.center(size) for _ in vals)
+        print '|'.join( errs )
+    print '='*len(firstline)
+    print '|'.join(centers)
+
+#def h2d_to_txt(histo, options):
+#    xbins = histo.GetNbinsX()
+#    ybins = histo.GetNbinsY()
+#    size  = 9
+#    centers = ['uflow'.center(size)]
+#    vals    = [prettyfloat(histo.GetBinContent(0), size)]
+#    errs    = [prettyfloat(histo.GetBinError(0), size)]
+#
+#    for i in xrange( 1, xbins+1 ):
+#        centers.append( prettyfloat( histo.GetXaxis().GetBinCenter(i), size) )
+#        vals.append( prettyfloat( histo.GetBinContent(i), size) )
+#        errs.append( prettyfloat( histo.GetBinError(i), size) )
+#
+#    centers.append( 'oflow'.center(size) )
+#    vals.append( prettyfloat( histo.GetBinContent(xbins+1), size) )
+#    errs.append( prettyfloat( histo.GetBinError(xbins+1), size) )
+#
+#    firstline = '|'.join(vals)
+#    print firstline
+#    if options.stat:
+#        print '|'.join( '+/-'.center(size) for _ in vals)
+#        print '|'.join( errs )
+#    print '='*len(firstline)
+#    print '|'.join(centers)
+#
+
+__doc__ = 'prints a histogram to screen'
+parser = OptionParser(description=__doc__)
+parser.add_option('--show-errors', '-s', action='store_true', default = False,
+                  help='show the statistical error', dest='stat')
+
+options, args = parser.parse_args()
+
+import ROOT
+fname = args[0]
+path  = args[1]
+
+tfile = ROOT.TFile.Open(fname)
+histo = tfile.Get(path)
+
+h1d_to_txt(histo, options)
