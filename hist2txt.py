7a039aa19b5209d55fcc65c9fd6d4dff28717f87
Mauro Verzetti <mverzett@cern.ch>
2014-11-27 03:18:38 -0600

    No idea, too much time passed from modification

--- hist2txt.py
+++ hist2txt.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+import array
 import os
 import sys
+import time
+from pdb import set_trace
 from optparse import OptionParser
 
 def odg(num):
@@ -35,7 +38,11 @@ def h1d_to_txt(histo, options):
     errs    = [prettyfloat(histo.GetBinError(0), size)]
 
     for i in xrange( 1, xbins+1 ):
-        centers.append( prettyfloat( histo.GetXaxis().GetBinCenter(i), size) )
+        if options.binrange:
+            centers.append( prettyfloat( histo.GetXaxis().GetBinLowEdge(i), size/2) + '-' + \
+                            prettyfloat( histo.GetXaxis().GetBinLowEdge(i) + histo.GetXaxis().GetBinWidth(i), size/2) )
+        else:
+            centers.append( prettyfloat( histo.GetXaxis().GetBinCenter(i), size) )
         vals.append( prettyfloat( histo.GetBinContent(i), size) )
         errs.append( prettyfloat( histo.GetBinError(i), size) )
 
@@ -77,10 +84,33 @@ def h1d_to_txt(histo, options):
 #    print '|'.join(centers)
 #
 
+def rebin(histo, binning):
+    if isinstance(binning, int):
+        histo.Rebin(binning)
+        return histo
+    elif isinstance(binning, (list, tuple)):
+        bin_array = array.array('d', binning)
+        return histo.Rebin(len(binning)-1, str(time.time), bin_array)
+    raise ValueError('Wrong binning value, only integers and lists/tuples are supported, got a %s' % type(binning))
+
+def project(histo, axis):
+    paxis = 'X'
+    if axis.upper() == 'X':
+        paxis = 'Y'
+
+    nbins = getattr(histo, 'GetNbins%s' % paxis)()
+    return getattr(histo, 'Projection%s' % axis.upper())(str(time.time()+1), 0, nbins+1)
+
 __doc__ = 'prints a histogram to screen'
 parser = OptionParser(description=__doc__)
 parser.add_option('--show-errors', '-s', action='store_true', default = False,
                   help='show the statistical error', dest='stat')
+parser.add_option('--bin-range', action='store_true', default = False,
+                  help='', dest='binrange')
+parser.add_option('--rebin', default = '', type=str,
+                  help='rebins the histogram', dest='rebin')
+parser.add_option('--project', default = '', type=str,
+                  help='rebins the histogram', dest='project')
 
 options, args = parser.parse_args()
 
@@ -90,5 +120,11 @@ path  = args[1]
 
 tfile = ROOT.TFile.Open(fname)
 histo = tfile.Get(path)
+
+if options.project:
+    histo = project(histo, options.project)
+
+if options.rebin:
+    histo = rebin(histo, eval(options.rebin))
 
 h1d_to_txt(histo, options)
