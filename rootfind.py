694d02449ab76577872c2a467cb5c95552e81bb7
Mauro Verzetti <mverzett@cern.ch>
2014-11-27 03:19:42 -0600

    do not read objects unless REALLY needed, much faster now

--- rootfind.py
+++ rootfind.py
@@ -5,18 +5,22 @@ import os
 from fnmatch import fnmatch
 from optparse import OptionParser
 import ROOT 
-
+from pdb import set_trace
 
 __author__  = "Mauro Verzetti (mauro.verzetti@cern.ch)"
 __doc__ = """script to look at the content of the root file:\n\n
 Usage: rootfind file.root [options]"""
 
 def GetContent(dir):
-    tempList = dir.GetListOfKeys()
-    retList = []
-    for it in range(0,tempList.GetSize()):
-       retList.append(tempList.At(it).ReadObj())
-    return retList
+    'does not read key content'
+    keys = dir.GetListOfKeys()
+    return [(getattr(ROOT, i.GetClassName()), i.GetName()) for i in keys]
+
+def inherits_from(test, target):
+    if test == target:
+        return True
+    return any( inherits_from(test, sub) 
+                for sub in target.__subclasses__())
 
 def rootfind( directory, dirName='', **kwargs ):
     if 'max_depth' in kwargs and kwargs['max_depth'] == 0:
@@ -25,20 +29,22 @@ def rootfind( directory, dirName='', **kwargs ):
         kwargs['max_depth'] -= 1
         
     dirContent = GetContent(directory)
-    for entry in dirContent:
-      path = os.path.join(dirName,entry.GetName())
+    for obj_type, obj_name in dirContent:
+      path = os.path.join(dirName,obj_name)
       if 'name' not in kwargs or fnmatch(path,kwargs['name']):
           if 'type' not in kwargs or \
              not kwargs['type'] or \
-             entry.InheritsFrom(kwargs['type']):
+             inherits_from(obj_type, getattr(ROOT, kwargs['type'])):
               addenda = ''
               if 'code' in kwargs and kwargs['code']:
+                  entry = directory.Get(obj_name)
                   for code in  kwargs['code'].split('$'):
                       toeval  = 'entry.%s' % code
                       addenda += '%s ' % eval(toeval)
               print path,addenda
-      if entry.InheritsFrom('TDirectory'):
-         subdirName = os.path.join(dirName,entry.GetName())
+      if inherits_from(obj_type, getattr(ROOT, kwargs['type'])):
+         subdirName = os.path.join(dirName,obj_name)
+         entry= directory.Get(obj_name)
          rootfind(entry, subdirName, **kwargs)
 
 def parse_options(arguments=sys.argv[1:]):
@@ -59,11 +65,13 @@ def parse_options(arguments=sys.argv[1:]):
 
 if __name__ == '__main__':
     (options,file_names) = parse_options()
-    tfiles = [ROOT.TFile.Open(name) for name in file_names if '.root' in name]
+    tfile_names = [name for name in file_names if '.root' in name]
     
     if not options.show_errors:
         ROOT.gErrorIgnoreLevel = ROOT.kError+1 #Suppress anything that is not an exception
 
-    for tfile in tfiles:
+    for name in tfile_names:
+        tfile = ROOT.TFile.Open(name)
         print "In %s:" % tfile.GetName()
         rootfind( tfile, **vars(options))
+        tfile.Close()
